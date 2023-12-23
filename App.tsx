36360df7f3c254a13575e73f04902cbf9a62a5d2
jacklange22 <skierjack22@gmail.com>
2023-12-23 17:49:27 -0500

    Add dependencies for react-native-community/viewpager, react-navigation/native, react-navigation/stack, react-native-gesture-handler, react-native-reanimated, and react-native-safe-area-context

--- App.tsx
+++ App.tsx
@@ -1,118 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+// App.tsx
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './src/screens/welcome.js';
+import SignupScreen from './src/screens/signup.js';
+import LoginScreen from './src/screens/login.js';
+import AuthLoadingScreen from './src/screens/AuthLoadingScreen'; // Import the AuthLoadingScreen
+import HomeScreen from './src/screens/homescreen.js';
+import ProfileScreen from './src/screens/profile.js';
+import TestingLogScreen from './src/screens/testinglog.js';
+import testingdata from './src/testingdata.json';
+import SkisInputFields from './src/screens/inputfieldscreen.js';
+import WaxLogScreen from './src/screens/waxinglog.js';
+import SkiInfo from './src/screens/skiInfo.js';
+import AddSkiForm from './src/screens/newSki.js';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createStackNavigator();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        {/* Set Welcome as the initial route */}
+        {/* If you are ready to use AuthLoadingScreen, you can uncomment these lines*/}
+        <Stack.Screen
+          name="AuthLoading"
+          component={AuthLoadingScreen}
+          options={{ headerShown: false }}
+        />
+        
+        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }}/>
+        {/* Uncomment these lines when you're ready to add these screens*/}
+        <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="TestingLog" component={TestingLogScreen} options={{ headerShown: false }}/> 
+        <Stack.Screen name="WaxLog" component={WaxLogScreen} options={{ headerShown: false }}/> 
+        <Stack.Screen name="SkiInfo" component={SkiInfo} options={{ headerShown: false }}/>  
+        <Stack.Screen name="newSki" component={AddSkiForm} options={{headerShown:false}}/>  
+
+        {/* Other screens would go here */}
+        {/*<Stack.Screen name="Input" component={SkisInputFields} options={{ headerShown: false }}/>*/}
+      </Stack.Navigator>
+      
+    </NavigationContainer>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
